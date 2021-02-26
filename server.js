f3913054198a1d79520ea8f9f318f942704785e9
nidhalnasri2019 <47346151+nidhalnasri2019@users.noreply.github.com>
2021-02-26 21:31:40 +0100

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 //const bodyParser= require('body-parser')
-const path = require('path')
-const config = require('config')
+const path = require("path");
+const config = require("config");
 
-const app = express()
+const app = express();
 
 //Bodyparser Middleware
 
@@ -13,33 +13,35 @@ app.use(express.json());
 
 //Db config
 //const db = require('./config/keys').mongoURI
-const db = config.get('mongoURI')
+const db = config.get("mongoURI");
 
 //connect to mongo
-mongoose.connect(db,
-{useNewUrlParser:true,
- useCreateIndex:true})
-.then(()=>console.log('Mongodb connected..'))
-.catch(err=>console.log(err))
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Mongodb connected.."))
+  .catch(err => console.log(err));
 
 // routes
-app.use('/api/items', require('./routes/api/items'))
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
+app.use("/api/items", require("./routes/api/items"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 //Serve static assets if in production
-if(process.env.NODE_ENV==='production'){
-    //Set static folder
-    app.use(express.static('client/build'));
-    app.get('*' , (req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{
-    console.log(`server is running in ${PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is running in ${PORT}`);
+});
 
 //
